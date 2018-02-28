955c924db6b6c0749c13f48f0768524fcd4d45f5
yaodongwork <502523948@qq.com>
2018-02-28 16:37:52 +0800

    第五章项目跟踪练习提交

---
+++ littlewechat/data/post.js
@@ -0,0 +1,32 @@
+var local_database = [
+  {
+    date: "Sep 18 2016",
+    title: '正是虾粉什么几把玩意儿',
+    imgSrc: 'http://www.yao-dong.com/blog/2017/06/06/vueGo/vueGO.jpg',
+    avatar: 'http://www.yao-dong.com/blog/2017/06/05/one/oo.jpg',
+    content: '任冬是哥小东东 哈啥好事阿第克啊开始的埃里克大家阿里山扩大拉萨孔家店卢卡斯的就卡拉',
+    reading: '1112',
+    collection: '96'
+  },
+  {
+    date: "Sep 18 2016",
+    title: '1正是虾粉什么几把玩意儿',
+    imgSrc: 'http://www.yao-dong.com/blog/2017/06/06/vueGo/vueGO.jpg',
+    avatar: 'http://www.yao-dong.com/blog/2017/06/05/one/oo.jpg',
+    content: '任冬是哥小东东 哈啥好事阿第克啊开始的埃里克大家阿里山扩大拉萨孔家店卢卡斯的就卡拉',
+    reading: '1112',
+    collection: '96'
+  },
+  {
+    date: "Sep 18 2016",
+    title: '2正是虾粉什么几把玩意儿',
+    imgSrc: 'http://www.yao-dong.com/blog/2017/06/06/vueGo/vueGO.jpg',
+    avatar: 'http://www.yao-dong.com/blog/2017/06/05/one/oo.jpg',
+    content: '任冬是哥小东东 哈啥好事阿第克啊开始的埃里克大家阿里山扩大拉萨孔家店卢卡斯的就卡拉',
+    reading: '1112',
+    collection: '96'
+  }
+];
+module.exports = {
+  postList: local_database
+}
--- littlewechat/pages/welcome/welcome.js
+++ littlewechat/pages/welcome/welcome.js
@@ -1,6 +1,16 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+  onTap: function () {
+    // wx.navigateTo({
+    //   url: '../posts/post',
+    // })
+    wx.redirectTo({
+      url: '../posts/post',
+    })
+  },
+  onUnload: function () {
+    console.log('onUnload')
+  },
+  onHide: function () {
+    console.log('onHide')
+  }
 })
